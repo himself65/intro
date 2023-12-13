e937e34d248e8d1de2f4ba783b6354e1633d04ac
Alex Yang <himself65@outlook.com>
2023-12-12 21:53:45 -0600

    fix: code

--- patches/dist/lib/builder.js
+++ patches/dist/lib/builder.js
@@ -398,6 +398,7 @@ import { connectMiddleware } from 'waku';
 const entries = import(path.resolve('${config.distDir}', '${config.entriesJs}'));
 export default async function handler(req, res) {
   connectMiddleware({ entries, ssr: true })(req, res, () => {
+    console.log('not handled', req.url);
     throw new Error('not handled');
   });
 }
