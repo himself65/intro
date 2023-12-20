09f01ac7cd4379a877817cb8d2026a3123dc9fca
Alex Yang <himself65@outlook.com>
2023-12-20 02:48:25 -0600

    feat: update description

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,26 @@
 import { Layout } from './Layout.js'
 import { ThemeButton } from './ThemeButton.js'
+import { Description } from './Description.js'
+import { Banner } from './Banner.js'
 
 const App = () => {
   return (
     <Layout>
-      <main
-        className="flex min-h-screen flex-col items-start px-5 bg-white dark:bg-gray-900"
+      <div
+        className="flex min-h-screen flex-col items-start px-5 bg-white dark:bg-gray-900 text-xl font-light dark:text-gray-300"
       >
-        <div
-          className="flex flex-row items-center w-full flex-1 px-20 text-center">
+        <main
+          className="flex flex-row items-center w-full flex-1">
           <div
-            className="flex flex-col items-center w-full flex-1 px-20 text-center">
-            <h1 className="text-3xl font-bold sm:text-5xl dark:text-gray-200">
-              Hey, I&apos;m Alex Yang
-            </h1>
-            <div
-              className="text-xl font-light pt-5 sm:pt-10 dark:text-gray-300"
-            >
-              A student and a software engineer.
-            </div>
+            className="flex flex-col items-center w-full flex-1">
+            <Description/>
           </div>
           <div className="self-start pt-5 sm:pt-10">
             <ThemeButton/>
           </div>
-        </div>
-      </main>
+        </main>
+        <Banner/>
+      </div>
     </Layout>
   )
 }
